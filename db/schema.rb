3b23cea76c90be066a68a64385e182742b8801f9
Dylan Matthews <dylan.matthews1649@gmail.com>
2017-01-18 19:47:01 -0600

    Add a comment section for each individual post!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109235155) do
+ActiveRecord::Schema.define(version: 20170119010429) do
 
   create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "author"
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 20170109235155) do
     t.datetime "updated_at",                   null: false
     t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
     t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+  end
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "body",             limit: 65535
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
   add_foreign_key "blog_posts", "authors"
