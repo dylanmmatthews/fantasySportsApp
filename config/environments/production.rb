d1ea5dfa8e165b08c4bb3782965a4ab37a1e2233
Dylan Matthews <dylan.matthews1649@gmail.com>
2017-01-28 12:30:06 -0600

    Allow images to load in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
